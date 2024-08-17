35d1b02b3f732762bd528307545a8410a47e5775
fatihatuzjohora <fatihatuzjohora@gmail.com>
2024-08-17 14:48:37 +0600

    base url changed

--- src/service/baseApi.ts
+++ src/service/baseApi.ts
@@ -1,2 +1,2 @@
-export const baseApi = 'http://localhost:5000/api';
-// export const baseApi = 'https://market-mingle-sigma.vercel.app/api';
+// export const baseApi = 'http://localhost:5000/api';
+export const baseApi = 'https://market-mingle-sigma.vercel.app/api';
